21df811824811741dec9e02a3db22a0111c34cd9
cybniv <5002075+cybniv@users.noreply.github.com>
2023-08-24 16:43:27 +0200

    Fix Uninitialized constant Puma::DSL::DefaultRackup
    https://github.com/puma/puma/issues/2989

--- rails/benchmarker/config/puma.rb
+++ rails/benchmarker/config/puma.rb
@@ -3,7 +3,7 @@ threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
 preload_app!
 
-rackup      DefaultRackup
+rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
